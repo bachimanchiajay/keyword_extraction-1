754f4a1d28ccdb67bcc8e033022691a5f969eaa1
AjaykumarAI <54697773+AjaykumarAI@users.noreply.github.com>
2023-03-07 07:45:24 -0600

    Update up.py

--- up.py
+++ up.py
@@ -24,4 +24,36 @@ for page_number in range(doc.page_count):
         value = match[1]
         print(f'{key}: {value}')
 
+        
+        import textract
+
+# Define the path to the PDF file
+pdf_path = "path/to/pdf/file.pdf"
+
+# Extract the text from the PDF file using Textract
+text = textract.process(pdf_path)
+
+# Split the text into lines
+lines = text.decode("utf-8").splitlines()
+
+# Initialize an empty dictionary to store the form data
+form_data = {}
+
+# Iterate over each line in the text
+for line in lines:
+    # Check if the line contains a key-value pair
+    if ":" in line:
+        # Split the line into key and value
+        key, value = line.split(":", 1)
+
+        # Strip leading and trailing whitespace from the key and value
+        key = key.strip()
+        value = value.strip()
+
+        # Add the key-value pair to the form data dictionary
+        form_data[key] = value
+
+# Print the extracted form data
+print(form_data)
+
 
