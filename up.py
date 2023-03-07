76054b1840ed94d7d6a5c1ebed0f52d4fcc8ce3a
AjaykumarAI <54697773+AjaykumarAI@users.noreply.github.com>
2023-03-07 09:49:32 -0600

    Update up.py

--- up.py
+++ up.py
@@ -1,4 +1,44 @@
 import fitz
+from PIL import Image
+from layoutparser import OCR, Document
+
+# Load the PDF file using PyMuPDF
+pdf_path = 'path/to/pdf/file.pdf'
+doc = fitz.open(pdf_path)
+
+# Convert each page in the PDF file to a JPEG image
+for page_number in range(doc.page_count):
+    page = doc[page_number]
+    zoom = 2.0
+    rotation = 0
+    trans = fitz.Matrix(zoom, zoom).preRotate(rotation)
+    pix = page.getPixmap(matrix=trans, alpha=False)
+    image = Image.frombytes('RGB', [pix.width, pix.height], pix.samples)
+    image_path = f'page_{page_number+1}.jpg'
+    image.save(image_path)
+
+    # Extract text and layout information using OCR
+    ocr_agent = OCR()
+    layout = ocr_agent.detect(image_path, return_response=True)
+
+    # Convert the layout information to a LayoutParser document
+    document = Document(layout=layout, page_size=(image.width, image.height))
+
+    # Add the text to the LayoutParser document
+    text = ocr_agent.detect(image_path)
+    page_layout = layout[0]
+    block = page_layout['layout']
+    text_block = block.filter_blocks(lambda b, _: b['type'] == 'text')
+    text_block.set(text=text, type='text')
+
+    # Print the text and layout information for the page
+    print(f'Page {page_number + 1}:')
+    print(f'Text: {text}')
+    print(f'Layout: {page_layout}')
+
+
+
+import fitz
 import re
 
 # Open the PDF file
