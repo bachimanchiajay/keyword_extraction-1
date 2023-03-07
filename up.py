ace9c8318982dbb147d9ac120b93381a2da2b6e2
AjaykumarAI <54697773+AjaykumarAI@users.noreply.github.com>
2023-03-07 04:53:38 -0600

    Update up.py

--- up.py
+++ up.py
@@ -1,34 +1,20 @@
-import fitz
+import PyPDF2
 
-# Load the PDF document
-doc = fitz.open('path/to/pdf/file.pdf')
+# Define the word to search for
+word_to_search = 'Your Word'
 
-# Define the text to search for
-text_to_search = 'Your Text'
+# Open the PDF file
+with open('path/to/pdf/file.pdf', 'rb') as f:
+    # Create a PDF reader object
+    reader = PyPDF2.PdfReader(f)
 
-# Define the right-side region
-right_region = [0.5, 0, 1, 1]
+    # Iterate over each page in the PDF file
+    for page in reader.pages:
+        # Get the page text
+        page_text = page.extract_text()
 
-# Iterate over each page in the document
-for page_number in range(doc.page_count):
-    # Get the page object
-    page = doc[page_number]
+        # Check if the word is in the page text
+        if word_to_search in page_text:
+            # Print the page number and location of the word
+            print(f'Word found on page {page.page_number} at location {page_text.index(word_to_search)}')
 
-    # Search for the text
-    search_results = page.search_for(text_to_search)
-
-    # Iterate over each search result
-    for search_result in search_results:
-        # Check if the search result is in the right-side region
-        if search_result[0] > page.MediaBox[2]*right_region[0] and \
-           search_result[1] > page.MediaBox[3]*right_region[1] and \
-           search_result[2] > page.MediaBox[2]*right_region[2] and \
-           search_result[3] > page.MediaBox[3]*right_region[3]:
-            # Get the text in the right-side region
-            right_text = page.get_textbox([page.MediaBox[2]*right_region[0],
-                                           page.MediaBox[3]*right_region[1],
-                                           page.MediaBox[2]*right_region[2],
-                                           page.MediaBox[3]*right_region[3]])
-
-            # Print the text in the right-side region
-            print(right_text)
