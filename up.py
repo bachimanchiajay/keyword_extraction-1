c87bdc173d828c2308997a804f0eb1a7295b63a4
AjaykumarAI <54697773+AjaykumarAI@users.noreply.github.com>
2023-03-07 07:40:56 -0600

    Update up.py

--- up.py
+++ up.py
@@ -1,34 +1,27 @@
 import fitz
 import re
 
-# Define the keyword to search for
-keyword = 'Your Keyword'
-
-# Compile the regular expression pattern
-regex = re.compile(keyword)
-
 # Open the PDF file
 doc = fitz.open('path/to/pdf/file.pdf')
+
+# Define the regular expression pattern for key-value pairs
+pattern = r'(\w+)\s*:\s*(.+)'
 
 # Iterate over each page in the document
 for page_number in range(doc.page_count):
     # Get the page object
     page = doc[page_number]
 
-    # Search for the keyword using the regular expression
-    matches = regex.finditer(page.get_text())
+    # Get the text on the page
+    text = page.get_text()
 
-    # Iterate over each match
+    # Search for key-value pairs in the text
+    matches = re.findall(pattern, text)
+
+    # Print the key-value pairs
     for match in matches:
-        # Get the rectangle of the match
-        rect = match.group(0).strip('\n').replace('\n', ' ').replace('\r', ' ')
-        rect = page.search_for(rect)[0]
-
-        # Get the text region of the keyword
-        text_region = fitz.Rect(rect.x0, rect.y0, rect.x1, rect.y1 + 10)
-        text = page.get_text("text", clip=text_region)
-
-        # Print the page number, location, and text region of the match
-        print(f'Page {page_number+1}: {match.start()} - {match.end()}\n{rect}\n{text}')
+        key = match[0]
+        value = match[1]
+        print(f'{key}: {value}')
 
 
