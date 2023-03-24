9b3977891dbef61d6161a41535c09238605a8c20
AjaykumarAI <54697773+AjaykumarAI@users.noreply.github.com>
2023-03-24 02:29:19 -0500

    Update keyword.py

--- keyword.py
+++ keyword.py
@@ -52,3 +52,58 @@ for index, row in pdf_page_ranges.iterrows():
         # Upload the image to S3
         with open(image_file_name, 'rb') as image_file:
             s3.upload_fileobj(image_file, bucket_name, f'path/to/save/{image_file_name}')
+            
+            
+            
+import boto3
+import layoutparser as lp
+from PIL import Image
+
+# Load your custom-trained Layout Parser model
+model_checkpoint = "YOUR_MODEL_CHECKPOINT"
+config_file = "YOUR_CONFIG_FILE"
+model = lp.Detectron2LayoutModel(config_path=config_file,
+                                  model_path=model_checkpoint,
+                                  label_map={0: "Text"})
+
+def extract_text_from_coordinates(image_file_path, coordinates):
+    # Initialize the Textract client
+    textract_client = boto3.client("textract", region_name="YOUR_AWS_REGION")
+
+    # Read the image file
+    with open(image_file_path, "rb") as image_file:
+        image_data = image_file.read()
+
+    # Analyze the document using Textract
+    response = textract_client.analyze_document(Document={"Bytes": image_data}, FeatureTypes=["TABLES", "FORMS"])
+
+    # Load the image for Layout Parser
+    image = Image.open(image_file_path)
+
+    # Predict the layout using your custom-trained model
+    layout = model.detect(image)
+
+    extracted_texts = []
+    for coord in coordinates:
+        # Create a block representing the field coordinates
+        field_block = lp.Rectangle(coord)
+
+        # Extract the text within the field coordinates
+        field_text = lp.Layout([t for t in layout if field_block.contains(t)])
+        extracted_texts.append(field_text.get_text())
+
+    return extracted_texts
+
+# Replace "YOUR_IMAGE_FILE_PATH" with the path to your image file
+image_file_path = "YOUR_IMAGE_FILE_PATH"
+
+# Replace with the coordinates of the fields you want to extract text from
+coordinates = [
+    [x0, y0, x1, y1],
+    [x2, y2, x3, y3],
+    # ...
+]
+
+extracted_texts = extract_text_from_coordinates(image_file_path, coordinates)
+print(extracted_texts)
+
