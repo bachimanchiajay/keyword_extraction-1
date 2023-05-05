3c3dccba015a54f5aef7bf9b75f6b67121603813
AjaykumarAI <54697773+AjaykumarAI@users.noreply.github.com>
2023-05-05 11:51:57 +0530

    Add files via upload

---
+++ gpt_webservice.py
@@ -0,0 +1,26 @@
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+import openai
+app = FastAPI()
+openai.api_key = "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"
+class ChatInput(BaseModel):
+    input_text: str
+class ChatOutput(BaseModel):
+    response: str
+@app.post("/chat", response_model=ChatOutput)
+def chat(input: ChatInput):
+    # Use OpenAI's API to generate a response
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=input.input_text,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+    response_text = response.choices[0].text.strip()
+    return {"response": response_text}
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=5000)
+
