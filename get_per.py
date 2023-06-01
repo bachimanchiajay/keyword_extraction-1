f35e298f6d092148d1d5537626495915301aa77a
AjaykumarAI <54697773+AjaykumarAI@users.noreply.github.com>
2023-06-01 08:30:19 -0500

    Update get_per.py

--- get_per.py
+++ get_per.py
@@ -1,6 +1,6 @@
 import json
 
-def get_text_in_range(file_path, search_string, left_range, above_range):
+def get_text_in_range(file_path, search_string, left_range, above_range, page_number):
     with open(file_path, 'r') as f:
         textract_output = json.load(f)
 
@@ -8,13 +8,18 @@ def get_text_in_range(file_path, search_string, left_range, above_range):
     all_texts = []
 
     for block in textract_output['Blocks']:
-        if block['BlockType'] in ['WORD', 'LINE']:
+        # Check if block is in the specified page
+        if block['BlockType'] in ['WORD', 'LINE'] and block['Page'] == page_number:
             text = block['Text']
             all_texts.append(block)
 
             # Save the block data if it matches the search string
             if text == search_string:
                 search_string_data = block
+
+    if search_string_data is None:
+        print(f'Search string "{search_string}" not found in the document.')
+        return None, None
 
     left_texts = [text_block['Text'] for text_block in all_texts
                   if abs(text_block['Geometry']['BoundingBox']['Top'] - search_string_data['Geometry']['BoundingBox']['Top']) < 0.01
@@ -29,7 +34,10 @@ def get_text_in_range(file_path, search_string, left_range, above_range):
 
 file_path = 'your_json_file.json'  # Replace this with your file path
 search_string = 'YourSearchString'  # Replace this with your search string
+page_number = 2  # Replace this with your desired page number
 
-left_text, above_text = get_text_in_range(file_path, search_string, 0.3, 0.2)
-print(f'Text to the left of "{search_string}": {left_text}')
-print(f'Text above "{search_string}": {above_text}')
+left_text, above_text = get_text_in_range(file_path, search_string, 0.2, 0.2, page_number)
+
+if left_text is not None and above_text is not None:
+    print(f'Text to the left of "{search_string}": {left_text}')
+    print(f'Text above "{search_string}": {above_text}')
