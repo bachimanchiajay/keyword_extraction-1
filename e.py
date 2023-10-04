7499a384e156b9b99aa1da86ddc49f2435e43c98
AjaykumarAI <54697773+AjaykumarAI@users.noreply.github.com>
2023-10-04 16:24:03 +0530

    Update e.py

--- e.py
+++ e.py
@@ -64,3 +64,71 @@ def riskCodeAndPercentageExtractor_continuous_corrected(text, riskCodeList, tok)
             return [riskCodeData, None]
 
 riskCodeAndPercentageExtractor_continuous_corrected
+
+
+import re
+
+def riskCodeAndPerc_strict_order_revised(text, riskCodeList, used_codes):
+    output = []
+    text = re.sub('[^A-Za-z0-9%.]+', ' ', text)
+    text = text.replace("at", "")
+    textSplit = text.split()
+    
+    idx = 0
+    while idx < len(textSplit):
+        part = textSplit[idx]
+        
+        if "%" in part:
+            # Check left
+            if idx > 0 and textSplit[idx-1] in riskCodeList and textSplit[idx-1] not in used_codes:
+                used_codes.append(textSplit[idx-1])
+                output.append([textSplit[idx-1], part])
+                idx += 1  # Move to next part after percentage
+                continue
+            # Check right
+            elif idx < len(textSplit) - 1 and textSplit[idx+1] in riskCodeList and textSplit[idx+1] not in used_codes:
+                used_codes.append(textSplit[idx+1])
+                output.append([textSplit[idx+1], part])
+                idx += 2  # Move to next part after risk code
+                continue
+        
+        idx += 1
+
+    # Ensure that each percentage is associated with only one risk code
+    used_percentages = []
+    unique_output = []
+    for pair in output:
+        if pair[1] not in used_percentages:
+            unique_output.append(pair)
+            used_percentages.append(pair[1])
+    
+    return unique_output
+
+def riskCodeAndPercentageExtractor_strict_order_final(text, riskCodeList, tok):
+    if text:
+        token = tok.lower()
+        riskCodeData = []
+        used_codes = []  # To keep track of risk codes that have been associated
+        
+        ind = text.lower().find(token)
+        newText = text[ind+len(token)+1:]
+        
+        # Adjusting the regex pattern again to correctly capture percentages
+        percentages = re.findall(r'(\d{1,3}\s*%)', newText)
+        
+        pairs = riskCodeAndPerc_strict_order_revised(newText, riskCodeList, used_codes)
+        
+        riskCode = [pair[0] for pair in pairs]
+        riskPercentage = [pair[1] for pair in pairs]
+        
+        return [riskCode, riskPercentage]
+    else:
+        text = re.sub('[^A-Za-z0-9%.]+', " ", text)
+        ind = text.lower().find(token)
+        newText = text[ind+len(token):]
+        newText = newText.split()
+        for i in newText:
+            if i in riskCodeList:
+                riskCodeData.append(i)
+        return [riskCodeData, None]
+
